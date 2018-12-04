d8faad21cca2b012713619d3010a0685a6961264
Darlysson Felipe <dfalves@br.ibm.com>
2018-12-04 17:27:47 -0200

    Grey Area First Commit

---
+++ js/animation_Saved__05_23.js
@@ -0,0 +1,48 @@
+var $ = 'jQuery';
+jQuery(function($) {
+
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable'),
+        $animateds = $('.animated');
+
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.size() == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+
+    // Check all animatables and animate them if necessary
+		$animatables.each(function(i) {
+       var $animatable = $(this);
+			if (($animatable.offset().top - 20) < offset-$(window).height()/100*10 && !(($animatable.offset().top + $animatable.height()) < $(window).scrollTop())) {
+        $animatable.toggleClass('animatable',false).toggleClass('animated',true);
+			}
+    });
+
+    // Check all animated and desanimate :( them if necessary
+		$animateds.each(function(i) {
+       var $animated = $(this);
+			if (($animated.offset().top) > offset || (($animated.offset().top + $animated.height()) < $(window).scrollTop())) {
+        $animated.toggleClass('animatable',true).toggleClass('animated',false);
+			}
+    });
+
+	};
+
+  // Hook doAnimations on scroll, and trigger a scroll
+	$(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+});
+
+jQuery(document).ready(function(){
+  jQuery('#fixed-event').hide();
+  jQuery('#first-event').show();
+  setTimeout(function(){
+    jQuery('#first-event').hide();
+    jQuery('#fixed-event').show();
+  },10000);
+});
